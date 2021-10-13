ca8e1bef956b03112b2ec15ec9f9781ab22671c4
Yury <yura.sviderko@live.com>
2021-10-13 22:58:12 +0300

    initial upload

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "hgYGyhjff39hfFH9hffh93pwg3f0jJFf"
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouter');
+require('dotenv').config();
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
