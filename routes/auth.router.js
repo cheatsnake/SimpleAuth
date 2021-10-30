dac03234bfd6689ef4cffa4fa166f9ca4a6da162
Yury <yura.sviderko@live.com>
2021-10-30 15:10:31 +0300

    update auth router

---
+++ routes/auth.router.js
@@ -0,0 +1,13 @@
+const Router = require('express');
+const router = new Router();
+const controller = require('../controllers/auth.controller');
+const {check} = require('express-validator');
+
+router.post('/registration', [
+    check('username', 'Username length need to be from 4 symbols to 12 symbols').isLength({min: 4, max: 12}),
+    check('password', 'Password length need to be from 6 symbols to 16 symbols').isLength({min: 6, max: 16})
+    ], controller.registration);
+router.post('/login', controller.login);
+router.get('/check', controller.checkServer);
+
+module.exports = router
